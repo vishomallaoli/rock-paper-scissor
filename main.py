cb4401c940a77a54e02dfd5c0b40ae063a15fcc7
Visho <85623826+vishomallaoli@users.noreply.github.com>
2023-10-14 03:24:52 -0500

    Improved the code and debugged a TERM error online 5.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import random
+import os
 import time
+import random
+from colorama import Fore, init
+os.environ['TERM'] = 'xterm'
+
+# Initialize colorama
+init(autoreset=True)
 
 # ASCII Art
 choices_art = [
@@ -32,36 +38,56 @@ choices_art = [
 choices = ["Rock", "Paper", "Scissors"]
 
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def play_rps(user_choice, scores):
-    print(f"You chose:\n{choices_art[user_choice]}")
+    # Show the user's choice text and art
+    print("You chose:")
+    print(choices_art[user_choice])
+    time.sleep(1)
+    clear_screen()
 
+    # Random computer choice
     comp_choice = random.randint(0, 2)
-    time.sleep(1)  # Pause for a moment before revealing computer's choice
-    print(f"Computer chose:\n{choices_art[comp_choice]}")
 
+    # Get line-by-line art for both choices
+    user_art_lines = choices_art[user_choice].split('\n')
+    comp_art_lines = choices_art[comp_choice].split('\n')
+
+    # Show both choices side-by-side
+    print("You chose:                           Computer chose:")
+    for ua, ca in zip(user_art_lines, comp_art_lines):
+        print(f"{ua:35} {ca}")
+
+    # Check result
     if user_choice == comp_choice:
-        result = "You've drawn."
+        result = f"{Fore.YELLOW}You've drawn."
     elif (user_choice - comp_choice) % 3 == 1:
-        result = "You've won."
-        scores['user'] += 1  # Update user score
+        result = f"{Fore.GREEN}You've won!"
+        scores['user'] += 1
     else:
-        result = "You've lost."
-        scores['computer'] += 1  # Update computer score
+        result = f"{Fore.RED}You've lost."
+        scores['computer'] += 1
 
     print(result)
-    print(f"Score: You {scores['user']} - {scores['computer']} Computer\n")
+    print(f"\n{Fore.CYAN}Score: You {scores['user']} - {scores['computer']} Computer\n")
     return scores
 
 
 # Main game logic
-scores = {'user': 0, 'computer': 0}  # Initialize scores
+scores = {'user': 0, 'computer': 0}
 
 while True:
+    print(
+        f"{Fore.BLUE}What do you choose? Type {Fore.WHITE}0{Fore.BLUE} for Rock, {Fore.WHITE}1{Fore.BLUE} for Paper, {Fore.WHITE}2{Fore.BLUE} for Scissors, or {Fore.WHITE}'q'{Fore.BLUE} to quit")
     try:
-        user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors, or 'q' to quit\n")
+        user_choice = input()
 
         if user_choice == 'q':
-            print("Thanks for playing! Final Score: You -", scores['user'], ", Computer -", scores['computer'])
+            print(
+                f"{Fore.MAGENTA}Thanks for playing! Final Score: You - {scores['user']}, Computer - {scores['computer']}")
             break
 
         user_choice = int(user_choice)
@@ -69,6 +95,7 @@ while True:
             raise ValueError
 
         scores = play_rps(user_choice, scores)
-        time.sleep(1)  # A small pause to let user read the result
+        input("Press Enter to continue...")  # Pause before clearing screen for next round
+        clear_screen()
     except ValueError:
-        print("Invalid input! Please enter a valid number (0, 1, or 2) or 'q' to quit.")
+        print(f"{Fore.RED}Invalid input! Please enter a valid number (0, 1, or 2) or 'q' to quit.")
